2729fffd56a098d0a037415cf58c36eb708a18fe
Jack Smith <jack.built.la@gmail.com>
2021-02-20 18:56:45 -0800

    Initial Commit

---
+++ SprintTimer-v2-exp1/SprintTimer_v2_exp1App.swift
@@ -0,0 +1,17 @@
+//
+//  SprintTimer_v2_exp1App.swift
+//  SprintTimer-v2-exp1
+//
+//  Created by Jack Smith on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SprintTimer_v2_exp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
